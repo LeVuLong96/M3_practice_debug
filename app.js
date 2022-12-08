b378c3eff8c7151f757a2ca2a475e7c70ca81287
LongVulee <longpra2@gmail.com>
2022-12-08 14:03:54 +0700

    debug

---
+++ app.js
@@ -0,0 +1,33 @@
+// const checkLicensePlateInHanoi = (licensePlate) => {
+//     const array = [29, 30, 31, 32, 33, 40];
+//     for (let value of array) {
+//         if(array.includes(licensePlate)) {
+//             console.log('ok')
+//         }
+//         console.log('no')
+//     }
+// }
+//
+// checkLicensePlateInHanoi(59,30);
+
+const checkLicensePlateInHanoi = (licensePlate) => {
+    const array = ['29', '30', '31', '32', '33', '40'];
+    for (const value of array) {
+        if (licensePlate.startsWith(value)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+const listLicensePlates = ["29P1-124123", "30L9-63568", "17B3-29013", "33H2-21231"];
+
+let listLicensePlateInHanois = [];
+
+for (const value of listLicensePlates) {
+    if (checkLicensePlateInHanoi(value)) {
+        listLicensePlateInHanois.push(value);
+    }
+}
+
+console.log("Các biển số thuộc Hà Nội: ", listLicensePlateInHanois);
